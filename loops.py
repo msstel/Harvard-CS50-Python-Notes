93ece75e4073f6a9c9623885683fbd4d174d5c36
Stella G <91384354+msstel@users.noreply.github.com>
2023-01-08 22:13:44 -0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,17 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+
+
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+for name in names:
+    print(name)
+
+
+
+
+name = "Harry"
+
+for character in name:
+    print(character)
---
+++ sequences.py
@@ -0,0 +1,13 @@
+name = "Harry"
+
+print(name[0])
+
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print(names)
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
